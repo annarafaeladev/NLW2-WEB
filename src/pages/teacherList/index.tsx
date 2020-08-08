dba3862e2387e8ef557ae42e4f5bdacf9dbe7892
rafaela <rafabispo53@gmail.com>
2020-08-07 21:04:21 -0300

    adjust styles and add components in TeacherList

--- src/pages/teacherList/index.tsx
+++ src/pages/teacherList/index.tsx
@@ -1,27 +1,40 @@
 import React from "react";
-import "./styles.css";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+
+import "./styles.css";
 
 function TeacherList() {
+  const options = [
+    { value: "Matemática", label: "Matemátia" },
+    { value: "Geografia", label: "Geografia" },
+    { value: "Inglês", label: "Inglês" },
+    { value: "História", label: "História" },
+  ];
+
+  const options_week = [
+    { value: "1", label: "Domingo" },
+    { value: "2", label: "Segunda-feira" },
+    { value: "3", label: "Terça-feira" },
+    { value: "4", label: "Quarta-feira" },
+    { value: "5", label: "Quinta-feira" },
+    { value: "6", label: "Sexta-feira" },
+    { value: "7", label: "Sabádo" },
+  ];
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Esses são os Proffys disponíveis">
         <form id="search-teacher">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Select name="subject" label="Matéria" options={options} />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={options_week}
+          />
+          <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
       <main>
